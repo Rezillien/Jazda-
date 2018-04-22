ce357d1dc5fd1dcebf022cd35315e7ef305b80c9
fifimajster <filipeyro@gmail.com>
2018-04-22 18:39:50 +0200

    added creating blocks by mouse click

---
+++ Assets/AddRoad.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AddRoad : MonoBehaviour
+{
+
+    public GameObject prefab;
+    Vector3 start_position;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            GameObject obj = Instantiate(prefab, new Vector2(position.x, position.y), Quaternion.identity) as GameObject;
+
+
+
+
+
+
+        }
+
+    }
+
+
+}
