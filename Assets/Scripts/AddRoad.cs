fa13f6f8db3cf9622220642b54582185372b747a
Tomasz <kurek.tom@gmail.com>
2018-07-31 09:52:36 +0200

    refactor AddRoad.cs

--- Assets/Scripts/AddRoad.cs
+++ Assets/Scripts/AddRoad.cs
@@ -6,56 +6,65 @@ using UnityEngine;
 public class AddRoad : MonoBehaviour
 {
 
-    public GameObject prefab;
-    Vector3 start_position;
+    public GameObject roadPrefab;
+    Vector3 startPosition;
 
 
     // Use this for initialization
     void Start()
     {
-
-
-
-
+        startPosition = new Vector3();
     }
 
     // Update is called once per frame
     void Update()
     {
-
         if (Input.GetMouseButtonDown(0))
         {
-            start_position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            print(Input.mousePosition.x);
-            print(Input.mousePosition.y);
-            print(Input.mousePosition.z);
-            start_position.z = 0;
+            startPosition = StartPositionOfTheRoad();
         }
-
-
         if (Input.GetMouseButtonUp(0))
         {
-            Vector3 end_position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            end_position.z = 0;
-
-
-            GameObject obj = Instantiate(prefab) as GameObject;
-
-            Transform transform = obj.GetComponent<Transform>();
-
-            Vector3 middle = (start_position + end_position) / 2;
-            float angle = Vector3.SignedAngle(
-                Vector3.right,
-                end_position - start_position,
-                Vector3.forward
-                );
-            float length = Vector3.Distance(start_position, end_position);
-
-            transform.Translate(middle);
-            transform.RotateAround(middle, Vector3.forward, angle);
-            transform.localScale += new Vector3(length, 0, 0);
+            Vector3 endPosition = EndPositionOfTheRoad();
+            CreateRoad(startPosition, endPosition);
         }
     }
 
+    private void CreateRoad(Vector3 startPosition, Vector3 endPosition)
+    {
+        GameObject roadObject = Instantiate(roadPrefab) as GameObject;
 
+        Transform roadTransform = roadObject.GetComponent<Transform>();
+
+        Vector3 middlePointOfRoad = (startPosition + endPosition) / 2;
+        float angle = Vector3.SignedAngle(
+            Vector3.right,
+            endPosition - startPosition,
+            Vector3.forward
+            );
+        float lengthOfRoad = Vector3.Distance(startPosition, endPosition);
+
+        TransformRoad(roadTransform, middlePointOfRoad, angle, lengthOfRoad);
+    }
+
+    private void TransformRoad(Transform roadTransform, Vector3 middlePointOfRoad, float angle, float lengthOfRoad)
+    {
+        roadTransform.Translate(middlePointOfRoad);
+        roadTransform.RotateAround(middlePointOfRoad, Vector3.forward, angle);
+        roadTransform.localScale += new Vector3(lengthOfRoad, 0, 0);
+    }
+
+    private Vector3 EndPositionOfTheRoad()
+    {
+        Vector3 endPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        endPosition.z = 0;
+        return endPosition;
+    }
+
+    private Vector3 StartPositionOfTheRoad()
+    {
+        Vector3 startPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        startPosition.z = 0;
+        return startPosition;
+    }
 }
